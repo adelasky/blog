b0f7bbdb7e9edeabfcfc916c4dbcc71b5443598d
adelasky <lumossky@outlook.com>
2022-05-17 11:21:56 +0800

    fix banner

--- source/js/sky.js
+++
@@ -1,4 +0,0 @@
-const bannerContainer = $("#banner");
-const bannerMask = $("#banner .mask");
-$(bannerContainer).css("background-image", "url()");
-$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
